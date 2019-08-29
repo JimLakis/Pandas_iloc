5bf681743304a93bc41c9472c56ed086b0dcc304
Jim <ejames.b1@gmail.com>
2019-08-29 14:11:07 -0700

    adding function querying row data types

--- 01_Dataframe_characteristics.py
+++ 01_Dataframe_characteristics.py
@@ -5,20 +5,40 @@ import pandas as pd
 
 
 def load_CSV():
+	''' Reading in the CSV file, creating a DataFrame'''
     df = pd.read_csv('uk-500.csv', delimiter=',')
     return df
 
+	
 def how_many_rows(df):
+	''' Determining the dimensions of df, rows by columns'''
     print(df.shape[:])
     
+	
 def header(df):
+	''' Listing the column headers of the df '''
     print(df.head(0))
+
+	
+def types_returned_from_rows(df):
+    ''' Returning data types for a single row verses multiple rows. '''
+    def single_row(df):
+        ''' Returning a single row's data type yields a Series '''
+        print(f"Type of a single row returned: {type(df.iloc[0])}")
+    
+    def multiple_rows(df):
+        ''' Returning multiple row's data type yields a DataFrame '''
+        print(f"Type of multiple rows returned: {type(df.iloc[0:4])}")
+        
+    single_row(df)
+    multiple_rows(df)
     
     
 def main():
     df = load_CSV()
     how_many_rows(df)
     header(df)
+	types_returned_from_rows(df)
     
 
 if __name__ == '__main__':
