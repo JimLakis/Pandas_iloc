2acf11eb67b8174c222aa2f32252274b1e096758
Jim <ejames.b1@gmail.com>
2019-08-31 14:01:14 -0700

    02_iloc....py - tidy up

--- 02_iloc_selection.py
+++ 02_iloc_selection.py
@@ -8,8 +8,8 @@ def load_CSV():
     df = pd.read_csv('uk-500.csv', delimiter=',')
     return df
 
-	
-def select_row(df):
+
+def select_rows(df):
     def single_row_return_series(df):
         ''' Selecting a single row.
             Output's data type is a Series. '''
@@ -24,16 +24,16 @@ def select_row(df):
         print(f"Selecting the second row: {df.iloc[[1]]}")
         print(type(df.loc[[1]])) # returns: <class 'pandas.core.frame.DataFrame'>
         
-    #single_row_return_series(df)
-    single_row_return_dataframe(df)
+    def select_multiple_rows(df):
+        ''' Selecting multiple rows.
+        Output's data type is a DataFrame. '''
+        print(f"Selecting the first ten rows: {df.iloc[0:11]}")
         
+    #single_row_return_series(df)
+    #single_row_return_dataframe(df)
+    select_multiple_rows(df)
 
-def select_multiple_rows(df):
-	''' Selecting multiple rows.
-		Output's data type is a DataFrame. '''
-	print(f"Selecting the first ten rows: {df.iloc[0:11]}")
-	
-	
+
 def select_columns(df):
     def single_column(df):
         pass
@@ -42,13 +42,13 @@ def select_columns(df):
         pass
     
     single_column(df)
-    #multiple_columns(df)
+    multiple_columns(df)
 
-		
+
 def main():
     df = load_CSV()
     #select_rows(df)
-    multiple_rows(df)
+    select_columns(df)
 
 
 if __name__ == "__main__":
