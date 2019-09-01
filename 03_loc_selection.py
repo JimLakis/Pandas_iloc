98a9da34c7f8bd50201c29c3e21e3d828f6a3f8f
Jim <ejames.b1@gmail.com>
2019-09-01 16:32:14 -0700

    03_loc_selection.py - adding select_on_row_values() function scaffolding

--- 03_loc_selection.py
+++ 03_loc_selection.py
@@ -1,4 +1,4 @@
-# Python 3.6
+# Python v3.6
 
 
 import pandas as pd
@@ -8,10 +8,27 @@ def load_CSV():
     df = pd.read_csv('uk-500.csv', delimiter=',')
     return df
 
+    
+def select_on_row_values(df):
+    def select_on_single_row_values_output_series(df):
+        pass
+    
+    def select_on_single_row_values_output_dataframe(df):
+        pass
+        
+    def select_on_multiple_row_values(df):
+        pass
 
+    select_on_single_row_values_output_series(df)
+    select_on_single_row_values_output_dataframe(df)
+    select_on_multiple_row_values(df)
+    
+    
 def main():
     df = load_CSV()
+    df.set_index("last_name", inplace=True)
+    select_on_row_values(df)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
