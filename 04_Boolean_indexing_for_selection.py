e87b8c18b5e9baf8034fd52755c80edf7a2cc484
Jim <ejames.b1@gmail.com>
2019-09-13 16:40:23 -0700

    Dataframe_indexing.py - expanding boolean mapping to selection, two step process

--- 04_Boolean_indexing_for_selection.py
+++ 04_Boolean_indexing_for_selection.py
@@ -12,11 +12,30 @@ def adding_ID_col(df):
     ''' Creating and adding a new column to the df '''
     df['id'] = [x for x in range(df.shape[0])]
     return df
+	
+	
+def selection_via_boolean_mapping_two_steps(df):
+    ''' Boolean mapping in two steps.
+        First create the boolean map Series.
+        Second utilize the mapping values to make selections. '''
+    def boolean_mapping(df):
+        ''' Producing a Series of mapping values, ie. True/False '''
+        s = df['county'] == 'Kent'
+        return s
+    
+    def loc_selection_based_on_mapping(s):
+        ''' Applying mapping values in loc[] selection.
+            Return value is a DataFrame.  '''
+        print(df.loc[s])
+    
+    s = boolean_mapping(df)
+    loc_selection_based_on_mapping(s)
 
 
 def main():
     df = load_CSV()
 	df.set_index("id", inplace = True)
+	selection_via_boolean_mapping_two_steps(df)
     
 
 if __name__ == '__main__':
