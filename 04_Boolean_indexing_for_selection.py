2da34debc33cb1fe8ae5557587bff6162f66edb5
Jim <ejames.b1@gmail.com>
2019-09-13 15:34:41 -0700

    04_Boolean_indexing_for_selection.py - adding id column, setting that column as index

--- 04_Boolean_indexing_for_selection.py
+++ 04_Boolean_indexing_for_selection.py
@@ -7,11 +7,16 @@ import pandas as pd
 def load_CSV():
     df = pd.read_csv('uk-500.csv', delimiter=',')
     return df
+	
+def adding_ID_col(df):
+    ''' Creating and adding a new column to the df '''
+    df['id'] = [x for x in range(df.shape[0])]
+    return df
 
 
 def main():
     df = load_CSV()
-	df.set_index("last_name", inplace = True)
+	df.set_index("id", inplace = True)
     
 
 if __name__ == '__main__':
