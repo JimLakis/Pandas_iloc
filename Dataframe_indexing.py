684c30854a5b2d89ebd0acc5c59fa48ec95f27d5
Jim <ejames.b1@gmail.com>
2019-09-13 15:10:34 -0700

    Dataframe_indexing.py - tidying up

--- Dataframe_indexing.py
+++ Dataframe_indexing.py
@@ -7,27 +7,27 @@ import pandas as pd
 def load_CSV():
     df = pd.read_csv('uk-500.csv', delimiter=',')
     return df
-	
-	
+    
+    
 def set_index_on_column_inplace(df):
     ''' NOTE: inplace = True can not be utilized within a function as in this example '''
-    # df.set_index("last_name", inplace=True)
-    # See below in main() for where indexing takes place inplace...
-    # Note: The column utilzed as the index now is NOT displayed in its normal position for display purposes.
-    # Note: inplace = True makes the changes to current dataframe. Without this argument, another df is created in memory.
-    pass
+    df.set_index("email", inplace=True)
 
 
 def set_index_on_column(df):
     ''' Setting the index WITHOUT inplace argument is acceptable within a function definition '''
-    df = df.set_index("last_name")
-    return df
+    df2 = df.set_index("last_name")
+    return df2
 
 
 def main():
     df = load_CSV()
     #df.set_index("last_name", inplace=True)
-    df = set_index_on_column(df)
+    set_index_on_column_inplace(df)
+    df2 = set_index_on_column(df)
+    
+    print(df.head(1))
+    print(df2.head(1))
 
 
 if __name__ == "__main__":
