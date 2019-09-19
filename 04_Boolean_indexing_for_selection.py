4a9a23498de11a2f5eb2770d11119b90b6da3fd3
Jim <ejames.b1@gmail.com>
2019-09-19 13:59:59 -0700

    04_Boolean_indexing_for_selection.py - adding selection_via_boolean_mapping_more_examples()

--- 04_Boolean_indexing_for_selection.py
+++ 04_Boolean_indexing_for_selection.py
@@ -35,6 +35,52 @@ def selection_via_boolean_mapping_two_steps(df):
 def selection_via_boolean_mapping_one_step(df):
     ''' Combining the creating of the boolean map and its use as an index for loc[] in one statement '''
     print(df.loc[df['county'] == 'Kent'])
+	
+	
+def selection_via_boolean_mapping_more_examples(df):
+    def map_and_search_display_one_column(df):
+        print(df.loc[df['first_name'] == 'Eric', 'company_name'])
+        # Single column argument is a naked string
+        # return type: Series
+        
+    def map_and_search_display_one_column2(df):
+        print(df.loc[df['first_name'] == 'Eric', ['company_name']])
+        # Single column argument enclosed in a list
+        # return type: DataFrame
+        
+    def map_and_search_include_only_certain_columns(df):
+        print(df.loc[df['first_name'] == 'Eric', ['company_name', 'email', 'phone1']])
+        # return type: DataFrame
+        
+    def select_on_name_and_column_range(df):
+        ''' Select rows where the first_name is 'Eric'.
+            Display the columns 'city' thru to 'emain'. '''
+        print(df.loc[df['first_name'] == 'Eric', 'city':'email'])
+    
+    def select_on_email_ending_with(df):
+        ''' Select rows based on email addresses ending with "hotmain.com" '''
+        # df['email'] # displays email addresses
+        # df['email'].str.endswith('hotmail.com') # displays boolean indexing values, true/false values
+        print(df.loc[df['email'].str.endswith('hotmail.com')]) # applying the boolean index map
+        
+    def select_on_name_being_in_a_list(df):
+        ''' Select rows based on first_name being in a list.
+            Include only thier company names. '''
+        print(df.loc[df['first_name'].isin(['Eric', 'Edgar', 'Michell']), 'company_name'])
+    
+    def select_on_values_from_different_columns(df):
+        ''' Select rows based on postal codes starting with "GL52" AND
+            county equal to "Gloucestershire".
+            Note: parentheses required around the second search phrase. '''
+        print(df.loc[df['postal'].str.startswith('GL52') & (df['county'] == 'Gloucestershire'), 'company_name'])
+
+    #map_and_search_display_one_column(df)
+    #map_and_search_display_one_column2(df)
+    #map_and_search_include_only_certain_columns(df)
+    #select_on_name_and_column_range(df)
+    #select_on_email_ending_with(df)
+    #select_on_name_being_in_a_list(df)
+    select_on_values_from_different_columns(df)
 
 
 def main():
@@ -42,7 +88,8 @@ def main():
 	df = adding_ID_col(df)
 	df.set_index("id", inplace = True)
 	#selection_via_boolean_mapping_two_steps(df)
-    selection_via_boolean_mapping_one_step(df)
+    #selection_via_boolean_mapping_one_step(df)
+	selection_via_boolean_mapping_more_examples(df)
 
 if __name__ == '__main__':
     main()
