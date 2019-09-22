3064fc854a30860ba85b33a8e40a788687f45843
Jim <ejames.b1@gmail.com>
2019-09-22 15:00:28 -0700

    05_Setting_values_with_loc.py - adding practice_retreiving_values(), just for practice.

--- 05_Setting_values_with_loc.py
+++ 05_Setting_values_with_loc.py
@@ -7,11 +7,29 @@ import pandas as pd
 def load_CSV():
     df = pd.read_csv('uk-500.csv', delimiter=',')
     return df
-    
-    
+
+
+def practice_retreiving_values(df):
+    def retrieve_rows(df):
+        ''' Returns all rows where 'county' equals Derbyshire, display all columns.
+            Output type is a Series. '''
+        return df.loc[df["county"] == "Derbyshire"]
+
+    def retrieve_rows_and_specific_column(df):
+        ''' Returns all rows where 'county' equals Derbyshire, display only the columns 'phone1'.
+            Output type is a DataFrame. '''
+        return df.loc[df["county"] == "Derbyshire", ["phone1"]]
+        
+    #results = retrieve_rows(df)
+    results = retrieve_rows_and_specific_column(df)
+    print(results)
+
+
 def main():
     df = load_CSV()
     df.set_index("last_name", inplace=True)
+
+	practice_retreiving_values(df)
 
 
 if __name__ == '__main__':
